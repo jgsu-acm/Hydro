9e87ca961b7e8e5c40c17f21f6d7613765a42643
Zxilly <zhouxinyu1001@gmail.com>
2021-11-13 20:07:06 +0800

    ci: fix build

--- docker/modify.js
+++ docker/modify.js
@@ -1,6 +1,6 @@
-const fs = require("fs")
-const PATH = "packages/"+ process.argv[2] +"/package.json"
-data_oj = JSON.parse(fs.readFileSync(PATH))
-data_ut = JSON.parse(fs.readFileSync("packages/utils/package.json"))
-data_oj["dependencies"]["@hydrooj/utils"] = data_ut["version"]
-fs.writeFileSync(PATH, JSON.stringify(data_oj, null, 4))
+const fs = require('fs');
+const PATH = `packages/${process.argv[2]}/package.json`;
+const data_oj = JSON.parse(fs.readFileSync(PATH).toString());
+const data_ut = JSON.parse(fs.readFileSync('packages/utils/package.json').toString());
+data_oj['dependencies']['@hydrooj/utils'] = data_ut['version'];
+fs.writeFileSync(PATH, JSON.stringify(data_oj, null, 4));
