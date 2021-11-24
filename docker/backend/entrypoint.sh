33a0347f0503945ee8d20c745ed14176e0faf1ec
StableAgOH <stagoh@qq.com>
2021-11-24 20:35:17 +0800

    feat: add sonic plugin

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -4,20 +4,24 @@ ROOT=/root/.hydro
 mkdir -p $ROOT
 
 if [ ! -f "$ROOT/addon.json" ]; then
-    echo '["@hydrooj/ui-default"]' > "$ROOT/addon.json"
+    hydrooj addon add @hydrooj/ui-default
+    hydrooj addon add @hydrooj/import-qduoj
+    hydrooj addon add @hydrooj/vjudge
+    hydrooj addon add @hydrooj/onlyoffice
+    hydrooj addon add @hydrooj/sonic
 fi
 
 if [ ! -f "$ROOT/config.json" ]; then
     # TODO 变成变量
-    echo '{"host": "oj-mongo", "port": "27017", "name": "hydro", "username": "", "password": ""}' > "$ROOT/config.json"
+    echo '{"host": "oj-mongo", "port": "27017", "name": "hydro", "username": "", "password": ""}' >"$ROOT/config.json"
 fi
 
 if [ ! -f "$ROOT/first" ]; then
-    echo "for marking use only!" > "$ROOT/first"
+    echo "for marking use only!" >"$ROOT/first"
     hydrooj cli system set file.accessKey "$ACCESS_KEY"
     hydrooj cli system set file.secretKey "$SECRET_KEY"
-    # TODO 变成变量
     hydrooj cli system set file.endPoint http://oj-minio:9000/
+    hydrooj cli system set sonic.host oj-sonic
 
     hydrooj cli user create systemadmin@systemjudge.local root rootroot 2
     hydrooj cli user setSuperAdmin 2
