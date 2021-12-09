f0547d91d53eeae0ff006cd50a7f1cff37ac31f8
AgOH <stagoh@qq.com>
2021-12-09 18:39:24 +0800

    feat(docker): increase sandbox memory limit

--- docker/judge/entrypoint.sh
+++ docker/judge/entrypoint.sh
@@ -5,5 +5,5 @@ mkdir -p $ROOT
 if [ ! -f "$ROOT/judge.yaml" ]; then
     cp /root/judge.yaml $ROOT
 fi
-pm2 start sandbox
+pm2 start "ulimit -s unlimited && /usr/bin/sandbox" sandbox
 pm2-runtime start hydrojudge
