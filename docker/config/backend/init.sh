cf81f3cad4592dd82594b9578392e880d134d607
AgOH <stagoh@qq.com>
2022-01-09 01:51:26 +0800

    docker: rename init.sh in Dockerfile

--- docker/config/backend/init.sh
+++ docker/config/backend/init.sh
@@ -8,5 +8,3 @@ hydrooj cli user create systemadmin@systemjudge.local root rootroot 2
 hydrooj cli user setSuperAdmin 2
 hydrooj cli user create systemjudge@systemjudge.local judger judgerjudger 3
 hydrooj cli user setJudge 3
-
-mv /root/.hydro/init.sh /root/.hydro/init.sh.used
