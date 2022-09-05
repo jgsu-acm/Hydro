8902a74f787aac0a462c6807096700603c1d4f7a
StableAgOH <stagoh17@gmail.com>
2022-09-05 05:50:28 +0000

    chore: enhance devcontainer [skip ci]

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,5 @@
 yarn install
 npx hydrooj cli system set server.port 2333
-npx hydrooj cli system set hydro-elastic.host oj-elastic
+npx hydrooj cli system set hydro-elastic.host elastic
 npx hydrooj cli user create root@hydro.local root rootroot 2
 npx hydrooj cli user setSuperAdmin 2
