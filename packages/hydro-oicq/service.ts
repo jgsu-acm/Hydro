4829e961ece55a1d059e6f0368fd9ee89e962e38
Zxilly <zxilly@outlook.com>
2022-09-30 04:41:13 +0000

    feat: support multi group

--- packages/hydro-oicq/service.ts
+++ packages/hydro-oicq/service.ts
@@ -15,7 +15,7 @@ const logger = new Logger('oicq');
 declare module 'hydrooj/src/interface' {
     interface SystemKeys {
         'hydro-oicq.account': number;
-        'hydro-oicq.groupId': number;
+        'hydro-oicq.groupId': string;
         'hydro-oicq.platform': number;
         'hydro-oicq.datadir': string;
     }
@@ -27,14 +27,19 @@ class OICQService implements BaseService {
     public started = false;
 
     private client: oicq.Client;
-    public group: oicq.Group;
+    public groups: oicq.Group[];
+
+    validGroup(groupID: number) {
+        const ids = this.groups.map((group) => group.group_id);
+        return ids.includes(groupID);
+    }
 
     async start() {
         try {
             const account = system.get('hydro-oicq.account');
             if (!account) throw Error('no account');
-            const groupId = system.get('hydro-oicq.groupId');
-            if (!groupId) throw Error('no groupId');
+            const groupId = system.get('hydro-oicq.groupId').split(',');
+            if (!groupId || groupId.length === 0) throw Error('no groupId');
             const platform = system.get('hydro-oicq.platform') || 1;
             const datadir = system.get('hydro-oicq.datadir') || `${os.homedir}/.hydro/oicq`;
 
@@ -61,20 +66,20 @@ class OICQService implements BaseService {
             });
             this.client.on('system.online', () => {
                 logger.info('Logged in!');
-                this.group = this.client.pickGroup(groupId, true);
+                this.groups = groupId.map((id) => this.client.pickGroup(parseInt(id, 10), true));
             });
             this.client.on('system.offline.kickoff', () => logger.warn('Kicked offline!'));
             this.client.on('system.offline.network', () => logger.warn('Network error causes offline!'));
             this.client.on('message.group', async (event) => {
-                if (event.group_id !== groupId) return;
+                if (!this.validGroup(event.group_id)) return;
                 const msgList = event.raw_message.split(' ');
                 const command = msgList[0];
-                if (command === '/help') this.help();
-                else if (command === '/contest') cli(msgList.slice(1).join(' '), '/contest', (s) => this.group.sendMsg(s));
+                if (command === '/help') this.help(event.group);
+                else if (command === '/contest') cli(msgList.slice(1).join(' '), '/contest', (s) => event.group.sendMsg(s));
             });
             this.client.on('notice.group.poke', (event) => {
-                if (event.group_id !== groupId) return;
-                this.help();
+                if (!this.validGroup(event.group_id)) return;
+                this.help(event.group);
             });
 
             this.client.login();
@@ -93,18 +98,18 @@ class OICQService implements BaseService {
         process.stdin.once('data', (password) => this.client.login(String(password).trim()));
     }
 
-    async help() {
-        this.sendMsg([
+    async help(group: oicq.Group) {
+        this.sendMsg(group, [
             '/contest: 查看最近 3 天各大 OJ 上的比赛信息，使用 /contest -h 查看关于此命令的更多信息',
             '/help: 查看此条帮助信息',
         ].map((s) => `${s}\n`), null, true);
     }
 
-    async sendMsg(messages: string[], url?: string, emoji = true) {
+    async sendMsg(group: oicq.Group, messages: string[], url?: string, emoji = true) {
         const tail = [];
         if (emoji) tail.push(emojis[Math.floor(emojis.length * Math.random())]);
         if (url) tail.push(url);
-        this.group.sendMsg([...messages, ...tail].join('\n'));
+        group.sendMsg([...messages, ...tail].join('\n'));
     }
 }
 
@@ -132,7 +137,7 @@ bus.on('record/judge', async (rdoc, updated) => {
     if (pdoc.hidden || pdoc.owner === rdoc.uid) return;
     const name = await getName(uid);
     messages.push(`${name} 刚刚 AC 了 ${pdoc.pid} ${pdoc.title}，orz！`);
-    await service.sendMsg(messages, prefix ? `${prefix}/p/${pdoc.pid || pdoc.docId}` : null);
+    await Promise.all(service.groups.map((group) => service.sendMsg(group, messages, prefix ? `${prefix}/p/${pdoc.pid || pdoc.docId}` : null)));
 });
 
 bus.on('contest/add', async (tdoc, docId) => {
@@ -151,7 +156,7 @@ bus.on('contest/add', async (tdoc, docId) => {
         messages.push(`赛制：${tdoc.rule}`);
         _url = prefix ? `${prefix}/contest/${docId}` : null;
     }
-    await service.sendMsg(messages, _url);
+    await Promise.all(service.groups.map((group) => service.sendMsg(group, messages, _url)));
 });
 
 bus.on('discussion/add', async (ddoc) => {
@@ -159,5 +164,10 @@ bus.on('discussion/add', async (ddoc) => {
     const messages: string[] = [];
     const name = await getName(ddoc.owner);
     messages.push(`${name} 刚刚创建了讨论：${ddoc.title}，快去看看吧~~~`);
-    await service.sendMsg(messages, prefix ? `${prefix}/discuss/${ddoc.docId}#${ddoc.updateAt.getTime()}` : null);
+    await Promise.all(
+        service.groups.map(
+            (group) =>
+                service.sendMsg(group, messages, prefix ? `${prefix}/discuss/${ddoc.docId}#${ddoc.updateAt.getTime()}` : null),
+        ),
+    );
 });
