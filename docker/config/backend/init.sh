5b5ed6c5c9f4608f53986411a1f57551e9f064c8
StableAgOH <stagoh17@gmail.com>
2022-10-11 05:18:01 +0000

    build: no longer use elasticsearch

--- docker/config/backend/init.sh
+++ docker/config/backend/init.sh
@@ -1,4 +1,4 @@
-hydrooj cli system set hydro-elastic.host oj-elastic
+# hydrooj cli system set hydro-elastic.host oj-elastic
 hydrooj cli user create systemadmin@systemjudge.local root rootroot 2
 hydrooj cli user setSuperAdmin 2
 hydrooj cli user create systemjudge@systemjudge.local judger judgerjudger 3
