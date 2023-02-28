74e9be7633520afd7265d17f7f57b25104ffdeb3
StableAgOH <stagoh@qq.com>
2023-02-28 20:29:26 +0000

    chore: fix devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,6 +1,11 @@
-git config --global --add safe.directory /workspace
+cfgdir="${HOME}/.hydro"
+if [ ! -f $cfgdir/config.json ]; then
+    echo '{"host":"mongo","port":"27017","name":"hydro","username":"","password":""}' >$cfgdir/config.json
+fi
+if [ ! -f $cfgdir/addon.json ]; then
+    echo '["@hydrooj/ui-default","@hydrooj/hydrojudge"]' >$cfgdir/addon.json
+fi
 yarn install
 npx hydrooj cli system set server.port 2333
-npx hydrooj cli system set hydro-elastic.host elastic
 npx hydrooj cli user create root@hydro.local root rootroot 2
 npx hydrooj cli user setSuperAdmin 2
