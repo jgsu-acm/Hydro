2469c126001ff8e32d63e93c9dd4c44deea2a7ff
StableAgOH <stagoh@qq.com>
2023-03-01 01:15:39 +0000

    chore: fix devcontainer [skip ci]

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,5 +1,5 @@
-echo '{"host":"mongo","port":"27017","name":"hydro","username":"","password":""}' >${HOME}/.hydro/config.json
-echo '["@hydrooj/ui-default","@hydrooj/hydrojudge"]' >${HOME}/.hydro/addon.json
+echo '{"host":"mongo","port":"27017","name":"hydro","username":"","password":""}' >$HOME/.hydro/config.json
+echo '["@hydrooj/ui-default","@hydrooj/hydrojudge"]' >$HOME/.hydro/addon.json
 yarn install
 npx hydrooj cli system set server.port 2333
 npx hydrooj cli user create root@hydro.local root rootroot 2
