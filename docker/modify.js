a82347e080c52bec84a571190e10b58b741b7083
AgOH <50107074+StableAgOH@users.noreply.github.com>
2024-03-20 16:44:59 +0000

    ci: fix

--- docker/modify.js
+++ docker/modify.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
+
+const dataRegister = JSON.parse(fs.readFileSync('packages/register/package.json').toString());
+const dataUtils = JSON.parse(fs.readFileSync('packages/utils/package.json').toString());
 for (let i = 2; i < process.argv.length; i++) {
     const path = `packages/${process.argv[i]}/package.json`;
-    const data_target = JSON.parse(fs.readFileSync(path).toString());
-    const data_utils = JSON.parse(fs.readFileSync('packages/utils/package.json').toString());
-    data_target['dependencies']['@hydrooj/utils'] = data_utils['version'];
-    fs.writeFileSync(path, JSON.stringify(data_target, null, 4));
+    const dataTarget = JSON.parse(fs.readFileSync(path).toString());
+    dataTarget['dependencies']['@hydrooj/utils'] = dataUtils['version'];
+    fs.writeFileSync(path, JSON.stringify(dataTarget, null, 4));
 }
+dataUtils['dependencies']['@hydrooj/register'] = dataRegister['version'];
+fs.writeFileSync('packages/utils/package.json', JSON.stringify(dataUtils, null, 4));
